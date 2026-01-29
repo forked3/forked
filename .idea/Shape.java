6a3be7ee3aeb02a961a343ac0b2d5599faf3527d
Abhi <binesh@ualberta.ca>
2026-01-28 19:49:35 -0700

    Abhi

--- .idea/Shape.java
+++ .idea/Shape.java
@@ -1,7 +1,7 @@
 public abstract class Shape(){
     int x;
     int y;
-    String color = "blue"
+    String color = "pink"
     public Shape (int x, int y){
         this.x = x;
         this.y = y;
