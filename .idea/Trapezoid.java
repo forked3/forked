7d00c5338b35edf49678e7712a7cea9e7f21eba5
acc4bosco <unknownlgamin@gmail.com>
2026-01-29 09:12:14 -0700

    added color

--- .idea/Trapezoid.java
+++ .idea/Trapezoid.java
@@ -1,7 +1,7 @@
 public class Trapezoid extends Shape{
     int topLength;
     int baseLength;
-
+    String color = "blue";
     public Trapizoid(int topLength, int baseLength) {
         this.topLength = topLength;
         this.baseLength = baseLength;
